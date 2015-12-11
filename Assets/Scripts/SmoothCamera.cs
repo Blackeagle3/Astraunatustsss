4f43421db088f2956decd29df23750eac1111ced
Blackeagle3 <dane.nilsson@gmail.com>
2015-12-11 17:37:07 +0100

    11/12 2015
    added camera smooth script and a couple of new textures, also updated
    map

---
+++ Assets/Scripts/SmoothCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothCamera : MonoBehaviour {
+    public Transform playerPos;
+    public float smoothDamp = 0.2f;
+    Vector3 zv2 = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 v2 = new Vector3(playerPos.position.x, playerPos.position.y, 0);
+        transform.position = Vector3.SmoothDamp(transform.position, v2, ref zv2, smoothDamp);
+	}
+}
