ca3fe6cabfd99e69b34ac457734e7c33d0b9cdcb
Blackeagle3 <dane.nilsson@gmail.com>
2016-01-16 20:12:42 +0100

    16/1 16
    Added firing script(slightly broken) and block script

--- Assets/Scripts/movementPlayer.cs
+++ Assets/Scripts/movementPlayer.cs
@@ -13,12 +13,17 @@ public class movementPlayer : MonoBehaviour
     public LayerMask whatIsGround;
     private bool grounded;
     private bool doubleJumped;
-    public Camera myCam;
-    
+    public RaycastHit rHit;
+    public float raycastMaxRange;
+    public GameObject drop;
+    public LineRenderer lR;
+    public Camera cam;
+    public float amountOfPoop;
 
     void Start()
     {
-        GetComponent<Camera>();
+        lR = gameObject.GetComponent<LineRenderer>();
+        lR.SetColors(Color.red, Color.green);
     }
 
     void FixedUpdate()
@@ -27,6 +32,12 @@ public class movementPlayer : MonoBehaviour
     }
     void Update()
     {
+        if (amountOfPoop == 3)
+        {
+            Application.LoadLevel();
+        }
+
+
         if (grounded)
             doubleJumped = false;
 
@@ -46,31 +57,60 @@ public class movementPlayer : MonoBehaviour
         moveVelocity = 0f;
 
         if (Input.GetKey(KeyCode.D))
-
         {
             moveVelocity = moveSpeed;
         }
 
         if (Input.GetKey(KeyCode.A))
-
         {
             moveVelocity = -moveSpeed;
         }
 
         GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
         if (GetComponent<Rigidbody2D>().velocity.x > 0)
             transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+
         else if (GetComponent<Rigidbody2D>().velocity.x < 0)
             transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
 
-        if (Input.GetKeyDown(KeyCode.Mouse1))
+        if (Input.GetKey(KeyCode.Mouse0))
         {
-            //Ray2D ray = new Ray2D(new Vector2(transform.position.x, transform.position.y), new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-            //RaycastHit2D cast = Physics2D.Raycast(ray.origin, ray.direction);
-            //Debug.DrawRay(new Vector3(transform.position.x, transform.position.y), new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+            lR.SetVertexCount(2);
+            
+
+            Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 2.5f;
+
+            Debug.DrawRay(transform.position, mousePos - transform.position);
+            lR.SetPosition(0, transform.position);
+            lR.SetPosition(1, mousePos);
 
 
-           // Camera myCam = GetComponent<Camera>();
+
+            RaycastHit2D hit = (Physics2D.Raycast(transform.position, mousePos - transform.position, Vector2.Distance(transform.position, mousePos)));
+            if (hit.transform.tag == "Destroyable")
+            {
+                Destroy(hit.collider.gameObject);
+                Instantiate(drop, hit.transform.position, Quaternion.identity);
+                print(hit.transform.tag + " Just got hit");
+            }
+        }
+
+        
+
+        else
+        {
+            lR.SetVertexCount(0);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.collider.tag == "poo")
+        {
+            Destroy(col.collider.gameObject);
+            print("U got mail *ding*");
+            amountOfPoop++;
         }
     }
 }
